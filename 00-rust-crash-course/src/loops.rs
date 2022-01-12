8ac0a02ac5b65e76238e6a58495e749d7d1705af
sCarita <gustavosilvacarita@gmail.com>
2022-01-12 16:59:08 +0000

    minor fix.

--- 00-rust-crash-course/src/loops.rs
+++ 00-rust-crash-course/src/loops.rs
@@ -1,33 +1,33 @@
 // Loops - Used to iterate until a condition is met
 
 pub fn run() {
-  let _count = 0;
+  let mut count = 0;
 
   // Infinite Loop
-  // loop {
-  //   count += 1;
-  //   println!("Number: {}", count);
+  loop {
+    count += 1;
+    println!("Number: {}", count);
 
-  //   if count == 20 {
-  //     break;
-  //   }
-  // }
+    if count == 20 {
+      break;
+    }
+  }
 
   // While Loop (FizzBuzz)
-  // while count <= 100 {
-  //   if count % 15 == 0 {
-  //     println!("fizzbuzz");
-  //   } else if count % 3 == 0 {
-  //     println!("fizz");
-  //   } else if count % 5 == 0 {
-  //     println!("buzz")
-  //   } else {
-  //     println!("{}", count);
-  //   }
-
-  //   // Inc
-  //   count += 1;
-  // }
+  count = 0;
+  while count <= 100 {
+    if count % 15 == 0 {
+      println!("fizzbuzz");
+    } else if count % 3 == 0 {
+      println!("fizz");
+    } else if count % 5 == 0 {
+      println!("buzz")
+    } else {
+      println!("{}", count);
+    }
+    // Inc
+    count += 1;
+  }
 
   // For Range
   for x in 0..100 {
